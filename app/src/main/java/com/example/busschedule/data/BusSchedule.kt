5eec06a201534803781a4776bbbba51692f13bb2
Xero kun <elo48858@gmail.com>
2023-07-06 15:51:19 +0100

    `FEAT` Setup schedule db entity

--- app/src/main/java/com/example/busschedule/data/BusSchedule.kt
+++ app/src/main/java/com/example/busschedule/data/BusSchedule.kt
@@ -15,8 +15,17 @@
  */
 package com.example.busschedule.data
 
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Schedule")
 data class BusSchedule(
+    @PrimaryKey
     val id: Int,
+    @ColumnInfo(name = "stop_name")
     val stopName: String,
+    @ColumnInfo(name = "arrival_time")
     val arrivalTimeInMillis: Int
 )
